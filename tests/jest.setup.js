90317c5e3c78ad9385b293018f5c403a894d1bf3
Michael Daniels <mdaniels5757@gmail.com>
2024-12-07 19:44:12 -0500

    Update jest.setup.js

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -7,7 +7,6 @@ mw.config.set({
 require('../morebits.js');
 require('../twinkle.js');
 require('../modules/twinklewarn.js');
-require('../modules/twinklexfd.js');
 global.Morebits = window.Morebits;
 
 global.assert = require('assert');
